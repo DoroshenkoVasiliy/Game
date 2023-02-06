4c2077cdda5d2e960b3b25f188d3bd19fecc2452
Vasiliy Doroshenko <dorosh.120304@mail.ru>
2023-02-06 21:59:54 +0300

    commit

--- script.js
+++ script.js
@@ -29,18 +29,3 @@ function stop() {
 function drawBg() {
   ctxMap.drawImage(background, 0, 0, 800, 800);
 }
-
-function myMove() {
-  var elem = document.getElementById("car1");
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.right = pos + "px";
-      elem.style.left = pos + "px";
-    }
-  }
-}
