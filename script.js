8925371f681d055c083a41238f3de9a03e428d6c
Vasiliy Doroshenko <dorosh.120304@mail.ru>
2023-02-13 17:41:20 +0300

    commit

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-window.onload = init;
-let map;
-let ctxMap;
-let gameWidth = 800;
-let gameHeight = 800;
-let background = new Image();
-background.src = "doroga.png";
-function init() {
-  map = document.getElementById("map");
-  ctxMap = map.getContext("2d");
-  map.width = gameWidth;
-  map.height = gameHeight;
-  drawBg();
-}
-let requestAnimFrame = window.requestAnimationFrame;
-let isPlaing;
-
-function Loop() {
-  if (isPlaing) {
+function func() {
+  y = random(600);
+  n = irandom(1);
+  if ((n = 0)) {
+    (x = -50), (hspeed = 10);
+  } else {
+    (x = 850), (hspeed = -10);
   }
-}
-function start() {
-  isPlaing = true;
-  Loop();
-}
-function stop() {
-  isPlaing = folse;
-}
-function drawBg() {
-  ctxMap.drawImage(background, 0, 0, 800, 800);
 }
